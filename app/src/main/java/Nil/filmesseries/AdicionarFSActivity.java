a559b5f36770805984f40cc9111cfc020e48df48
Nil Silva <nil_silva_12@hotmail.com>
2019-03-28 07:58:06 +0000

    Verificação dos dados da activity para adicionar um filme/serie. Internacionalização.

--- app/src/main/java/Nil/filmesseries/AdicionarFSActivity.java
+++ app/src/main/java/Nil/filmesseries/AdicionarFSActivity.java
@@ -1,14 +1,15 @@
 package Nil.filmesseries;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class AdicionarFSActivity extends AppCompatActivity {
 
@@ -22,7 +23,74 @@ public class AdicionarFSActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void AddCancelar(View view) {
+    public void AddCancelarF(View view) {
         finish();
+    }
+
+    public void AddGuardarF(View view) {
+        int flag = 0;
+        RadioGroup RG = findViewById(R.id.radioGroupAddF);
+        int checked = RG.getCheckedRadioButtonId();
+        TextView menErro = findViewById(R.id.textViewAddFormErr);
+
+        if(checked == -1){
+            menErro.setText(getString(R.string.menErr));
+            flag = 1;
+        }
+        EditText editTextCampo = findViewById(R.id.editTextAddNome);
+        String textoCampo = editTextCampo.getText().toString();
+
+        if(textoCampo.trim().length() == 0){
+
+            editTextCampo.setError(getString(R.string.menErr));
+            editTextCampo.requestFocus();
+            flag = 1;
+        }
+
+        editTextCampo = findViewById(R.id.editTextAddData);
+        textoCampo = editTextCampo.getText().toString();
+
+        if(textoCampo.trim().length() == 0){
+
+            editTextCampo.setError(getString(R.string.menErr));
+            editTextCampo.requestFocus();
+
+            flag = 1;
+        }
+
+        editTextCampo = findViewById(R.id.editTextAddNum);
+        textoCampo = editTextCampo.getText().toString();
+
+        if(textoCampo.trim().length() == 0){
+
+            editTextCampo.setError(getString(R.string.menErr));
+            editTextCampo.requestFocus();
+
+            flag = 1;
+        }
+
+        editTextCampo = findViewById(R.id.editTextAddEpiVistos);
+        textoCampo = editTextCampo.getText().toString();
+
+        if(textoCampo.trim().length() == 0){
+
+            editTextCampo.setError(getString(R.string.menErr));
+            editTextCampo.requestFocus();
+
+            flag = 1;
+        }
+
+        Spinner spin = findViewById(R.id.spinnerAddF);
+        String spinSel = spin.getSelectedItem().toString();
+
+        if(spinSel.equals(getString(R.string.SelEstado))){
+            Toast.makeText(this, getString(R.string.SelEstado), Toast.LENGTH_SHORT).show();
+            flag = 1;
+        }
+
+        if(flag == 0){
+            finish();
+            Toast.makeText(this, getString(R.string.Sucesso), Toast.LENGTH_SHORT).show();
+        }
     }
 }
