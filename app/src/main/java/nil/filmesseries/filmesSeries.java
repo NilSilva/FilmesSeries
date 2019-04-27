fb51da3cc98577271b60ed0b6e1ae9d034428cac
Nil Silva <nil_silva_12@hotmail.com>
2019-04-27 07:57:49 +0100

    Classe para representar os filmes e series com os sets e gets.

---
+++ app/src/main/java/nil/filmesseries/filmesSeries.java
@@ -0,0 +1,61 @@
+package nil.filmesseries;
+
+import java.util.Date;
+
+public class filmesSeries {
+
+    private String formato;
+    private String nome;
+    private int nEpisodios;
+    private int nEpiVistos;
+    private Date data;
+    private String status;
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getnEpisodios() {
+        return nEpisodios;
+    }
+
+    public void setnEpisodios(int nEpisodios) {
+        this.nEpisodios = nEpisodios;
+    }
+
+    public int getnEpiVistos() {
+        return nEpiVistos;
+    }
+
+    public void setnEpiVistos(int nEpiVistos) {
+        this.nEpiVistos = nEpiVistos;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
