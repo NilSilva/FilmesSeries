c08dd3ba88f43521695354b02d8e89c7f332d4f8
Nil Silva <nil_silva_12@hotmail.com>
2019-04-27 08:03:26 +0100

    Classe para representar as pessoas com os sets e gets.

---
+++ app/src/main/java/nil/filmesseries/Pessoas.java
@@ -0,0 +1,34 @@
+package nil.filmesseries;
+
+import java.util.Date;
+
+public class Pessoas {
+
+    private String nome;
+    private String trabalho;
+    private Date dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTrabalho() {
+        return trabalho;
+    }
+
+    public void setTrabalho(String trabalho) {
+        this.trabalho = trabalho;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
