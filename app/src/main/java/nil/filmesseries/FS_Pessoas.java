fc7b02e7bf3dea4cc3181ce05440b3c672528af0
Nil Silva <nil_silva_12@outlook.com>
2019-05-06 17:01:46 +0100

    Classe FS_Pessoas criada. funções getContentValues para a classe FS_Pessoas funções fromCursor para a classe FS_Pessoas

---
+++ app/src/main/java/nil/filmesseries/FS_Pessoas.java
@@ -0,0 +1,53 @@
+package nil.filmesseries;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+public class FS_Pessoas {
+
+    private long ID_FS;
+    private long ID_P;
+
+    public long getID_FS() {
+        return ID_FS;
+    }
+
+    public void setID_FS(long ID_FS) {
+        this.ID_FS = ID_FS;
+    }
+
+    public long getID_P() {
+        return ID_P;
+    }
+
+    public void setID_P(long ID_P) {
+        this.ID_P = ID_P;
+    }
+
+    public ContentValues getContentValues() {
+        ContentValues valores = new ContentValues();
+
+        valores.put(BdTable_FS_Pessoas.CAMPO_ID_FS, ID_FS);
+        valores.put(BdTable_FS_Pessoas.CAMPO_ID_PESSOAS, ID_P);
+
+        return valores;
+    }
+
+    public static FS_Pessoas fromCursor(Cursor cursor) {
+
+        long id_FS = cursor.getLong(
+                cursor.getColumnIndex(BdTable_FS_Pessoas.CAMPO_ID_FS)
+        );
+
+        long id_P = cursor.getLong(
+                cursor.getColumnIndex(BdTable_FS_Pessoas.CAMPO_ID_PESSOAS)
+        );
+
+        FS_Pessoas FS_P = new FS_Pessoas();
+
+        FS_P.setID_FS(id_FS);
+        FS_P.setID_P(id_P);
+
+        return FS_P;
+    }
+}
